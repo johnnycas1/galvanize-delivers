7dd4364a0f6362f1093bed5be55e59fc16b76419
Johnny Castillo <johnnycast@gmail.com>
2018-04-30 15:19:36 -0600

    some changes made

---
+++ main.js
@@ -0,0 +1,29 @@
+$(function() {
+    console.log( "ready!" );
+});
+
+$("#royal").click(function(){
+$("tr").append("td");
+console.log(result);
+
+});
+
+
+
+// $("#aragula").click(function(){
+// $("#price2").append("$11.99");
+// $("#item2").append("Aragula Pie");
+//   console.log('clicked')
+// });
+//
+// $("#smoke").click(function(){
+// $("#price3").append("$14.99");
+// $("#item3").append("Smoke Swine");
+//   console.log('clicked')
+// });
+//
+// $("#ice").click(function(){
+// $("#price4").append("$7.99");
+// $("#item4").append("Ice Cream Biscuit");
+//   console.log('clicked')
+// });
