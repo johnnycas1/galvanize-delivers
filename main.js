481ad75cae721a1af140881fa96fa205c2ed069c
Johnny Castillo <johnnycast@gmail.com>
2018-05-18 18:22:18 -0600

    many fixes on index.html

--- main.js
+++ main.js
@@ -1,29 +1,60 @@
 $(function() {
-    console.log( "ready!" );
+  'use strict';
+  var subtotal = 0;
+  var tax = 0;
+
+  $('.button-collapse').sideNav();
+
+  $('.card-action').click(function(e) {
+    e.preventDefault();
+
+    // create row with 3 cells
+    var tr = $('<tr>');
+    var td1 = $('<td>');
+
+    // set text of first cell to the title of the card clicked
+    td1.html($('span.card-title', e.target.parentNode.parentNode).html());
+    var td2 = $('<td>');
+    var td3 = $('<td>');
+
+    // set text of last cell to the price of the card clicked
+    td3.html($('p.price', e.target.parentNode.parentNode).html());
+
+    // append cells to the row.
+    $(tr).append(td1);
+    $(tr).append(td2);
+    $(tr).append(td3);
+
+    // append rows to the body of the table
+    $('#order-table tbody').append(tr); // .append($td);
+
+    var str = $(td3).html();
+    var res = str.substring(1, str.length);
+
+    subtotal += parseFloat(res);
+    var newSubtotal = subtotal.toFixed(2);
+
+    tax = parseFloat(res);
+
+    // total = parseFloat(res);
+    var total = (subtotal + tax).toFixed(2);
+
+    tax = (newSubtotal * 0.09).toFixed(2);
+
+    $('#subtotal').html(newSubtotal);
+    $('#tax').html(tax);
+    $('#total').html(total);
+
+    return;
+  });
 });
 
-$("#royal").click(function(){
-$("tr").append("td");
-console.log(result);
+$(function() {
+  'use strict';
+  $('#details-form').on('submit', function(e) {
+    var $toastContent = $('<span>Your order was placed successfully.</span>');
 
+    Materialize.toast($toastContent, 5000);
+    e.preventDefault();
+  });
 });
-
-
-
-// $("#aragula").click(function(){
-// $("#price2").append("$11.99");
-// $("#item2").append("Aragula Pie");
-//   console.log('clicked')
-// });
-//
-// $("#smoke").click(function(){
-// $("#price3").append("$14.99");
-// $("#item3").append("Smoke Swine");
-//   console.log('clicked')
-// });
-//
-// $("#ice").click(function(){
-// $("#price4").append("$7.99");
-// $("#item4").append("Ice Cream Biscuit");
-//   console.log('clicked')
-// });
